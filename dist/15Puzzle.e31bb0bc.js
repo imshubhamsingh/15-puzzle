c7f8a6ef1241ab28641cc3ac5448cf123a604189
Shubham Singh <imshubhamsingh97@gmail.com>
2018-11-20 20:29:37 +0530

    sw

--- dist/15Puzzle.e31bb0bc.js
+++ dist/15Puzzle.e31bb0bc.js
@@ -33051,7 +33051,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51511" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60441" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- dist/manifest.90f2fcd3.js
+++ dist/manifest.90f2fcd3.js
@@ -112,32 +112,32 @@ module.exports = {
   "display": "standalone",
   "description": "The 15 Puzzle, also known as Game of Fifteen.",
   "icons": [{
-    "src": "/android-icon-36x36.png",
+    "src": "/icon/android-icon-36x36.png",
     "sizes": "36x36",
     "type": "image/png",
     "density": "0.75"
   }, {
-    "src": "/android-icon-48x48.png",
+    "src": "/icon/android-icon-48x48.png",
     "sizes": "48x48",
     "type": "image/png",
     "density": "1.0"
   }, {
-    "src": "/android-icon-72x72.png",
+    "src": "/icon/android-icon-72x72.png",
     "sizes": "72x72",
     "type": "image/png",
     "density": "1.5"
   }, {
-    "src": "/android-icon-96x96.png",
+    "src": "/icon/android-icon-96x96.png",
     "sizes": "96x96",
     "type": "image/png",
     "density": "2.0"
   }, {
-    "src": "/android-icon-144x144.png",
+    "src": "/icon/android-icon-144x144.png",
     "sizes": "144x144",
     "type": "image/png",
     "density": "3.0"
   }, {
-    "src": "/android-icon-192x192.png",
+    "src": "/icon/android-icon-192x192.png",
     "sizes": "192x192",
     "type": "image/png",
     "density": "4.0"
@@ -170,7 +170,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51511" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60441" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
